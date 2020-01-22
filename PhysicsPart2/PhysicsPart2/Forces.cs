7f3562bbdb4afdfe78b2cb166cc6dfe4dbddfd29
ericaleb97 <43590510+ericaleb97@users.noreply.github.com>
2020-01-21 17:37:45 -0800

    Add files via upload

---
+++ PhysicsPart2/PhysicsPart2/Forces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsPart2
+{
+    public class Forces
+    {
+        private double _mass;
+        private double _force;
+        private double _acceleration;
+
+        public double Mass
+        {
+            get { return _mass; }
+        }
+
+        public double Force
+        {
+            get { return _force; }
+        }
+
+        public double Acceleration
+        {
+            get { return _acceleration; }
+        }
+
+        public Forces(double acceleration, double mass)
+        {
+            _acceleration = acceleration;
+            _mass = mass;
+        }
+
+        public void LawofForces()
+        {
+            _force = _mass * _acceleration;
+        }
+    }
+}
---
+++ PhysicsPart2/PhysicsPart2/TestingForce.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PhysicsPart2
+{
+    [TestFixture]
+    public class TestingForce
+    {
+        [TestCase]
+        public void Forces()
+        {
+            double mass = 155;
+            double acceleration = 9.18;
+
+            var forces = new Forces(acceleration, mass);
+
+            forces.LawofForces();
+
+            double actual = forces.Force;
+            double expected = 1422.9;
+            Assert.AreEqual(expected‬, actual, 0.000000000001);
+        }
+    }
+}
